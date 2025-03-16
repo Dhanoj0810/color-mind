ce8e19615f9109a1bb5cf8d049f3fc2e627f7123
Dhanoj0810 <142652835+Dhanoj0810@users.noreply.github.com>
2025-03-16 22:50:24 +0530

    updated to multiple colors

--- js/game.js
+++ js/game.js
@@ -1,14 +1,28 @@
 class ColorGame {
   constructor() {
-    this.colors = { black: "white" };
+    this.colors = {
+      black: "white",
+      red: "green",
+      blue: "yellow",
+      yellow: "blue",
+      green: "red",
+      purple: "orange",
+      orange: "purple",
+      pink: "brown",
+      brown: "pink",
+      gray: "white",
+    };
     this.presentColors = {};
     this.point = 0;
+    this.iterations = 0;
+    this.maxIterations = 10;
+    this.timer = null;
   }
 
   generateColorCombination() {
-    const color = Object.keys(this.colors).join("");
-
-    return [color, this.colors[color]];
+    const colorKeys = Object.keys(this.colors);
+    const randomColor = colorKeys[Math.floor(Math.random() * colorKeys.length)];
+    return [randomColor, this.colors[randomColor]];
   }
 
   updatePoint() {
@@ -23,36 +37,50 @@ class ColorGame {
     this.presentColors[key] = value;
   }
 
+  removePresentColor(color) {
+    const key = this.#findKey(color);
+    delete this.presentColors[key];
+    return key;
+  }
+
   #findKey(value) {
     const combinations = Object.entries(this.presentColors);
     const key = combinations.reduce(
       (key, combination) => (combination[1] === value ? combination[0] : key),
       ""
     );
-
-    return key;
-  }
-
-  removePresentColor(color) {
-    const key = this.#findKey(color);
-
-    delete this.presentColors[key];
-
     return key;
   }
 }
 
 const addElement = (parent, [bgColor, textColor]) => {
   const div = document.createElement("div");
-
   div.setAttribute("class", bgColor);
   div.textContent = textColor;
-
   parent.appendChild(div);
 };
 
 const removeElement = (className) => {
-  document.querySelector(`.${className}`).remove();
+  const element = document.querySelector(`.${className}`);
+  if (element) element.remove();
+};
+
+const removeAllElement = (parent) => {
+  parent.replaceChildren();
+};
+
+const startColorCycle = (arena, gameSession) => {
+  intervalId = setInterval(() => {
+    const colorCombination = gameSession.generateColorCombination();
+    addElement(arena, colorCombination);
+    gameSession.addPresentColor(colorCombination);
+    gameSession.iterations += 1;
+    if (gameSession.iterations > gameSession.maxIterations) {
+      clearInterval(intervalId);
+      alert(`Game Over! Your score: ${gameSession.point}`);
+      removeAllElement(arena);
+    }
+  }, 4000);
 };
 
 const startGame = (e) => {
@@ -60,9 +88,7 @@ const startGame = (e) => {
   const arena = document.getElementById("arena");
   const input = document.querySelector("input");
 
-  const colorCombination = gameSession.generateColorCombination();
-  addElement(arena, colorCombination);
-  gameSession.addPresentColor(colorCombination);
+  startColorCycle(arena, gameSession);
 
   input.addEventListener("change", (e) => {
     if (Object.values(gameSession.presentColors).includes(input.value)) {
@@ -77,8 +103,7 @@ const startGame = (e) => {
 
 const gameSetUp = () => {
   const button = document.getElementById("controller");
-
   button.addEventListener("click", startGame);
 };
 
-window.onload = startGame;
+window.onload = gameSetUp;
